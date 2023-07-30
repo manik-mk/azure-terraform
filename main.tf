e072a80a0813305aaedac34d1510854c05762233
manik-mk <manibharathi612@gmail.com>
2023-07-30 20:56:46 +0530

    backend removed

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "MANI-1"
-    storage_account_name = "masa0001"
-    container_name       = "tstate"
-    key                  = "BCQEZtWYpT3+WbtMZj/JgRjgqyVtUd7OG1MBtriIYxv99q2RVSg9jr3Y94YMxW9MoXQ45nelhUCu+AStaHCNUg=="
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "MANI-1"
+  #   storage_account_name = "masa0001"
+  #   container_name       = "tstate"
+  #   key                  = "BCQEZtWYpT3+WbtMZj/JgRjgqyVtUd7OG1MBtriIYxv99q2RVSg9jr3Y94YMxW9MoXQ45nelhUCu+AStaHCNUg=="
+  # }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
